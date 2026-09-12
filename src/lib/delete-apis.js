50676ae8a179e0dbae4fcb22e786a3a277c75801
Masum75-max <almasumabdullah414@gmail.com>
2026-09-13 03:34:28 +0600

    FIXING Callings

--- src/lib/delete-apis.js
+++ src/lib/delete-apis.js
@@ -47,7 +47,7 @@ export const deleteDoctorById = async (id) => {
   }
 };
 export const deleteReview = async (reviewId) => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
 
   const res = await fetch(`${baseUrl}/api/reviews/${reviewId}`, {
     method: "DELETE",
